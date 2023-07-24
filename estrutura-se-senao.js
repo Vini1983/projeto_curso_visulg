cebb21fa36493c71057a7cc37bd8eba69bb2e542
unknown <viniciusdesouza9@gmail.com>
2023-07-24 11:34:58 -0300

    Primeiro commit do projeto visualg

---
+++ estrutura-se-senao.js
@@ -0,0 +1,33 @@
+
+
+function acaoBotao() {
+    valor01 = prompt("Digite o primeiro valor: ")
+    operacao = prompt("Digite a operação: Ex: +, -, *, /")
+    valor02 = prompt("Digite o segundo valor: ")
+
+switch (operacao)
+      case "+":  
+        resultado = parseInt ( valor01 ) + parseInt( valor02 )
+        break;
+      case "-":  
+        resultado = parseInt ( valor01 ) - parseInt( valor02 )
+        break;
+      case "*":  
+        resultado = parseInt ( valor01 ) * parseInt( valor02 )
+        break;
+      case "/":  
+        resultado = parseInt ( valor01 ) / parseInt( valor02 )
+        break;
+document.getElementById("paragrafo").innerText = resultado
+}
+
+
+
+    
+
+
+ 
+  
+
+
+                
